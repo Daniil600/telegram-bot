322d1f64478206eb795733f87126abbef2ba9848
Daniil600 <ter6062@ya.ru>
2023-05-18 14:10:27 +0300

    Begin of Bot

---
+++ src/main/java/pro/sky/telegrambot/service/TelegramBotService.java
@@ -0,0 +1,9 @@
+package pro.sky.telegrambot.service;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+@Component
+public class TelegramBotService extends TelegramLongPollingBot {
+
+}
